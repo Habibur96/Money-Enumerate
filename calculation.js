efa98e26fb34e8e4a2ec49a1b5c71d2b2343ec89
Habibur96 <habiburrahmannayan66@gmail.com>
2022-04-24 15:52:56 +0600

    calculation added

--- calculation.js
+++ calculation.js
@@ -1,3 +1,6 @@
+
+
+
 // Task of Calculate Button
 document.getElementById('calculate').addEventListener('click', function () {
 
@@ -5,19 +8,19 @@ document.getElementById('calculate').addEventListener('click', function () {
     const Food = document.getElementById('food')
     Food.value
     const changeTypeOfFood = parseFloat(Food.value)
-    Food.value = ''
+    //  Food.value = ''
 
     //Expenses of Rent
     const Rent = document.getElementById('rent')
     Rent.value
     const changeTypeOfRent = parseFloat(Rent.value)
-    Rent.value = ''
+    // Rent.value = ''
 
     //Expenses of Clothes
     const Clothes = document.getElementById('clothes')
     Clothes.value
     const changeTypeOfClothes = parseFloat(Clothes.value)
-    Clothes.value = ''
+    // Clothes.value = ''
 
     //Sum of Expenses
     const sumOfExpenses = changeTypeOfFood + changeTypeOfRent + changeTypeOfClothes;
@@ -33,8 +36,8 @@ document.getElementById('calculate').addEventListener('click', function () {
     document.getElementById('totalExpenses').innerText = changeTypeOfSumOfExpenses
 
     // Calculated Balance
-    const tOtalExpenses = changeTypeOfIncome - changeTypeOfSumOfExpenses;
-    const changeTypeOfTotal = parseFloat(tOtalExpenses)
+    const totalExpenses = changeTypeOfIncome - changeTypeOfSumOfExpenses;
+    const changeTypeOfTotal = parseFloat(totalExpenses)
     // console.log(changeTypeOfTotal)
 
     //Added Balance 
@@ -44,4 +47,50 @@ document.getElementById('calculate').addEventListener('click', function () {
     //Call the function of value()
     value(changeTypeOfIncome, changeTypeOfTotal)
 
+
 })
+
+
+//Task of Save Button
+function value(changeTypeOfIncome, changeTypeOfTotal) {
+    document.getElementById('save-button').addEventListener('click', function () {
+
+        //Taken value for calculate the saving amount on Income
+        const saveInput = document.getElementById('save-input')
+        saveInput.value
+        //   console.log('********' + saveInput.value)
+        const changeTypeOfSaveInputValue = parseFloat(saveInput.value)
+        //saveInput.value = ''
+
+
+        //  console.log('->' + changeTypeOfIncome)
+        ///   console.log('->' + changeTypeOfTotal)
+
+
+        // console.log('----<<<<<<<<' + changeTypeOfSaveInputValue)
+        // Calculated percentage on saving amount.
+        const parsentSaving = (changeTypeOfSaveInputValue * changeTypeOfIncome) / 100
+        console.log('---->>' + parsentSaving)
+
+        const changeTypeOfParsentSaving = parseFloat(parsentSaving)
+
+
+
+        //Added Saving Amount
+        document.getElementById('savingAmount').innerText = changeTypeOfParsentSaving
+        console.log(changeTypeOfParsentSaving)// problrm-1
+
+        //Caculated and added the Remaining Balance
+        const remainingBalance = changeTypeOfTotal - changeTypeOfParsentSaving
+        const changeTypeOfRemainingBalance = parseFloat(remainingBalance)
+        console.log(changeTypeOfRemainingBalance)
+
+
+        document.getElementById('remaining-balance').innerText = changeTypeOfRemainingBalance
+
+        console.log(changeTypeOfParsentSaving)
+        console.log(changeTypeOfRemainingBalance)
+
+    })
+
+}
